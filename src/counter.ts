8b7d1681e65c48ce9ec5309ce975a3d893f5232e
Matheus Henrique <matherique@gmail.com>
2022-11-05 20:40:26 -0300

    fix; change to show 0 if date passed

--- src/counter.ts
+++ src/counter.ts
@@ -2,6 +2,11 @@ import dayjs from "dayjs";
 
 export function counter(start: dayjs.Dayjs, end: dayjs.Dayjs): [number, number, number, number] {
   const distance = end.diff(start, "milliseconds")
+
+  if (distance <= 0) {
+    return [0, 0, 0, 0]
+  }
+
   const daysCount = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hoursCount = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutesCount = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
