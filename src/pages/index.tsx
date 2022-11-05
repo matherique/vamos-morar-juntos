8d17d702f101f6d2db144626b37734334441e482
Matheus Henrique <matherique@gmail.com>
2022-11-05 19:41:19 -0300

    feat: added layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-screen h-screen bg-black flex justify-center ">
+      <div className="flex flex-row gap-24 mt-32">
+        <div className="flex flex-col text-center">
+          <span className="text-red-800 text-9xl">59</span>
+          <span className="text-white uppercase text-5xl">Days</span>
+        </div>
+        <div className="flex flex-col text-center">
+          <span className="text-red-800 text-9xl">23</span>
+          <span className="text-white uppercase text-5xl">Hours</span>
+        </div>
+        <div className="flex flex-col text-center">
+          <span className="text-red-800 text-9xl">59</span>
+          <span className="text-white uppercase text-5xl">Minutes</span>
+        </div>
+        <div className="flex flex-col text-center">
+          <span className="text-red-800 text-9xl">59</span>
+          <span className="text-white uppercase text-5xl">Seconds</span>
+        </div>
+      </div>
+    </div>
   )
 }
