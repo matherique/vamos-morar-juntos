d2e78fd114b2bf59f6f7960a1879039bfcce245c
Matheus Henrique <matherique@gmail.com>
2022-11-06 11:20:36 -0300

    feat: move numbers to component and rotate in mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,13 @@ type Props = {
   s: number
 }
 
-export const getStaticProps: GetServerSideProps<Props> = async () => {
-  const now = dayjs()
-  const [d, h, m, s] = counter(now, target)
-
-  return {
-    props: { d, h, m, s },
-  }
+function ClockNumber({ value, label }: { value: number, label: string }) {
+  return <div className="flex flex-col text-center max-sm:rotate-90 max-sm:my-6">
+    <span className="text-red-800 lg:text-9xl text-8xl">{String(value).padStart(2, '0')}</span>
+    <span className="text-white uppercase lg:text-5xl text-2xl">{label}</span>
+  </div>
 }
+
 
 export default function Home({ d, h, m, s }: Props) {
   const [days, setDays] = useState(d)
@@ -44,24 +43,21 @@ export default function Home({ d, h, m, s }: Props) {
 
   return (
     <div className="w-full h-screen bg-black flex justify-center ">
-      <div className="lg:flex flex-row lg:gap-24 lg:mt-32 mt-8">
-        <div className="flex flex-col text-center">
-          <span className="text-red-800 lg:text-9xl text-8xl">{String(days).padStart(2, '0')}</span>
-          <span className="text-white uppercase lg:text-5xl text-4xl">Days</span>
-        </div>
-        <div className="flex flex-col text-center">
-          <span className="text-red-800 lg:text-9xl text-8xl">{String(hours).padStart(2, '0')}</span>
-          <span className="text-white uppercase lg:text-5xl text-4xl">Hours</span>
-        </div>
-        <div className="flex flex-col text-center">
-          <span className="text-red-800 lg:text-9xl text-8xl">{String(minutes).padStart(2, '0')}</span>
-          <span className="text-white uppercase lg:text-5xl text-4xl">Minutes</span>
-        </div>
-        <div className="flex flex-col text-center">
-          <span className="text-red-800 lg:text-9xl text-8xl">{String(seconds).padStart(2, '0')}</span>
-          < span className="text-white uppercase lg:text-5xl text-4xl"> Seconds</span>
-        </div>
+      <div className="lg:flex flex-row lg:gap-24 lg:mt-32">
+        <ClockNumber value={days} label="days" />
+        <ClockNumber value={hours} label="hours" />
+        <ClockNumber value={minutes} label="minutes" />
+        <ClockNumber value={seconds} label="seconds" />
       </div>
     </div>
   )
+}
+
+export const getStaticProps: GetServerSideProps<Props> = async () => {
+  const now = dayjs()
+  const [d, h, m, s] = counter(now, target)
+
+  return {
+    props: { d, h, m, s },
+  }
 }
