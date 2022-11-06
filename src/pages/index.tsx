ba7ad060375231d3087bd22195b3f1a763ea9be9
Matheus Henrique <matherique@gmail.com>
2022-11-06 11:44:39 -0300

    feat: added more metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,11 +45,15 @@ export default function Home({ d, h, m, s }: Props) {
   return (
     <div>
       <Head>
-        <title>Vamos morar juntos</title>
+        <title>Vamos morar juntos - Matheus e Tainara ❤️</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Vamos morar juntos" />
-        <meta property="og:description" content="Contador de quanto tempo vai demorar para podermos morar juntos" />
-        <meta property="og:image" content="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⏳</text></svg>" />
+        <meta name="title" content="Vamos morar juntos - Matheus e Tainara❤️" />
+        <meta name="description" content="Contador de quanto tempo vai demorar para Matheus e Tainara morarem juntos 'sozinhos'" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.vamosmorarjuntos.com/" />
+        <meta property="og:title" content="Vamos morar juntos - Matheus e Tainara❤️" />
+        <meta property="og:description" content="Contador de quanto tempo vai demorar para Matheus e Tainara morarem juntos 'sozinhos'" />
 
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⏳</text></svg>" />
       </Head>
